fabe439b40a35c4b964481f664d782deba7b56cb
vishnu <kallumadivishuvardhan@gmail.com>
2025-07-09 15:09:01 +0530

    new

---
+++ listprogram.py
@@ -0,0 +1,5 @@
+l=list(map(int,input().split()))
+l.reverse()
+print(l)
+print("sum: ",sum(l),end=" ")
+print("avg: ",sum(l)//len(l))
---
+++ printprogram.py
@@ -0,0 +1,3 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+print(f"Hello, {name}! You will be {age+5} years old.")
